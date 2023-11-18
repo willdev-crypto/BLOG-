272e64f318e751dd8209d304785c50b6a2103968
Will.Co <120841470+willdev-crypto@users.noreply.github.com>
2023-11-18 12:04:54 -0300

    Delete Downloads directory

--- Downloads/blog-main (3)/blog-main/scripts/mobile_menu.js
+++
@@ -1,8 +0,0 @@
-function mobileMenuShow(){
-    let menu = document.querySelector(".mobile-menu");
-    if(menu.classList.contains("open")){
-        menu.classList.remove("open");
-    }else{
-        menu.classList.add("open");
-    }
-}
